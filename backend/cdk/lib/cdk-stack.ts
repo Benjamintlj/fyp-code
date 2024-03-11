5cbfa53f6f9e2a0ef36effe4d6c792e9049a8766
n0928518 <n0928518@my.ntu.ac.uk>
2024-03-11 09:38:51 +0000

    s3 bucket exists

---
+++ backend/cdk/lib/cdk-stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const lessonBucket = new s3.Bucket(this, 'fypLessonBucket', {
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+  }
+}
